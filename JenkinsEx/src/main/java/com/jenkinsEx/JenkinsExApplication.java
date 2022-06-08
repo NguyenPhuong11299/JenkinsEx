51feaaff6ce5c06dfb82c2b2c1966aeef4ad79c9
Phuong <93914914+NguyenPhuong11299@users.noreply.github.com>
2022-06-08 13:42:27 +0700

    add main

--- JenkinsEx/src/main/java/com/jenkinsEx/JenkinsExApplication.java
+++ JenkinsEx/src/main/java/com/jenkinsEx/JenkinsExApplication.java
@@ -1,4 +1,14 @@
 package com.jenkinsEx;
 
+import java.util.Scanner;
+
 public class JenkinsExApplication {
+    public static void main(String[]  args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so thu nhat: ");
+        int a = sc.nextInt();
+        System.out.print("Nhap so thu hai: ");
+        int b = sc.nextInt();
+        System.out.println("tong = " + (a+b));
+    }
 }
