e1ee25a76a55c6cd91aa2e50c992b470c2c2af53
Phuong <93914914+NguyenPhuong11299@users.noreply.github.com>
2022-06-09 12:00:15 +0700

    test fail

--- TestNgDemo/src/main/java/firstPackage/SecondClass.java
+++ TestNgDemo/src/main/java/firstPackage/SecondClass.java
@@ -1,15 +1,43 @@
 package firstPackage;
 
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SecondClass {
-    @Test
-    public void firstTest(){
-        System.out.println("First test method");
+    private WebDriver driver;
+    @BeforeTest
+    public void beforeTest() {
+        System.setProperty("webdriver.gecko.driver", "D:\\luyenTap\\chromeDriver\\geckodriver.exe");
+//        driver = new FirefoxDriver();
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+        driver = new FirefoxDriver(capabilities);
     }
-
     @Test
-    public void secondTest(){
-        System.out.println("Second test method");
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+//    @Before
+//    public void beforeTest() {
+//        System.setProperty("webdriver.gecko.driver", "D:\\luyenTap\\chromeDriver\\geckodriver.exe");
+////        driver = new FirefoxDriver();
+//        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//        capabilities.setCapability("marionette", true);
+//        driver = new FirefoxDriver(capabilities);
+//    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
     }
 }
